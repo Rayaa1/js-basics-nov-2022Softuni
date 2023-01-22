84da7260e9695204ded43e0dd4fd37dbde9f53d8
Raya Koleva <117087939+Rayaa1@users.noreply.github.com>
2023-01-22 18:44:30 +0200

    Delete 09.weatherForecast.js

--- exercises/09.weatherForecast.js
+++
@@ -1,8 +0,0 @@
-function weatherForecast(input){
-    let weather = input[0];
-    if(weather === "sunny"){
-        console.log("It's warm outside!")
-    }else{
-        console.log("It's cold outside!");
-    }
-}
